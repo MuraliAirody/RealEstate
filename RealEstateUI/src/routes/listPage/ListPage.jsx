68851dd849e53dbec5d7552dd62ea09b3571596c
MuraliAirody <muraliairody@gmail.com>
2024-04-02 12:49:41 +0530

    list of places

--- RealEstateUI/src/routes/listPage/ListPage.jsx
+++ RealEstateUI/src/routes/listPage/ListPage.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from "react";
+import Card from "../../components/card/Card";
+import "./listPage.scss";
+import { listData } from "../../lib/dummyData";
+import Filter from "../../components/filter/Filter";
 
 function ListPage() {
+  const data = listData;
+
   return (
-    <div>
-      
+    <div className="listPage">
+      <div className="listContainer">
+        <div className="wrapper">
+          <Filter />
+          {data.map((item) => (
+            <Card key={item.id} item={item} />
+          ))}
+        </div>
+      </div>
+      <div className="mapContainer">{/* <Map items={data} /> */}</div>
     </div>
-  )
+  );
 }
 
-export default ListPage
+export default ListPage;

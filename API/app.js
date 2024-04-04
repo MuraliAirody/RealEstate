ea65c04be8fdb1ce9a2bbd570948cb447f4bac92
MuraliAirody <muraliairody@gmail.com>
2024-04-04 21:11:57 +0530

    register API integration

--- API/app.js
+++ API/app.js
@@ -2,13 +2,15 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js"
 import dotenv from "dotenv"
+import cors from "cors"
 
 const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 dotenv.config()
-
+console.log(process.env.CLIENT_URL);
 app.use("/api/auth",authRoute)
 
 

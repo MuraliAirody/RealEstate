40611726608402cacd313bf23dae5b3f44b40350
MuraliAirody <muraliairody@gmail.com>
2024-04-04 20:56:03 +0530

    login, register

--- RealEstateUI/src/App.jsx
+++ RealEstateUI/src/App.jsx
@@ -4,6 +4,8 @@ import ListPage from "./routes/listPage/ListPage";
 import Layout from "./routes/layout/Layout";
 import SinglePage from "./routes/singlePage/SinglePage";
 import ProfilePage from "./routes/profilePage/ProfilePage";
+import Login from "./routes/login/Login";
+import Register from "./routes/register/Register";
 function App() {
   const router = createBrowserRouter([
     {
@@ -26,6 +28,14 @@ function App() {
           path:"/profile",
           element:<ProfilePage/>
         },
+        {
+          path:"/login",
+          element:<Login/>
+        },
+        {
+          path:"/register",
+          element:<Register/>
+        }
       ],
     },
   ]);

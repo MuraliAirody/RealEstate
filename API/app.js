e8f77ab724c6b829a97517e6f02bd67f80a23c1a
MuraliAirody <muraliairody@gmail.com>
2024-04-04 18:51:56 +0530

    schema created using prisma

--- API/app.js
+++ API/app.js
@@ -1,9 +1,13 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js"
+import dotenv from "dotenv"
 
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
+dotenv.config()
 
 app.use("/api/auth",authRoute)
 

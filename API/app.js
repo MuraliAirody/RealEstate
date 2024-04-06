08c53bb006aa1395d2c9a44a43f309fc0fee293a
MuraliAirody <muraliairody@gmail.com>
2024-04-07 00:34:36 +0530

    user API

--- API/app.js
+++ API/app.js
@@ -2,18 +2,22 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js"
 import testRoute from "./routes/test.route.js"
+import userRoute from "./routes/user.route.js"
 import dotenv from "dotenv"
 import cors from "cors"
 
 const app = express();
 
+dotenv.config()
+
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
-dotenv.config()
 
 app.use("/api/auth",authRoute)
 app.use("/api/test", testRoute);
+app.use("/api/users", userRoute);
 
 
 

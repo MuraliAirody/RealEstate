810dce1c83076de1bbe8a83d6af3c077a5de23ad
MuraliAirody <muraliairody@gmail.com>
2024-04-09 00:35:09 +0530

    post APIs

--- API/app.js
+++ API/app.js
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js"
 import testRoute from "./routes/test.route.js"
 import userRoute from "./routes/user.route.js"
+import postRoute from "./routes/post.route.js"
 import dotenv from "dotenv"
 import cors from "cors"
 
@@ -18,6 +19,8 @@ app.use(cors({origin:process.env.CLIENT_URL,credentials:true}))
 app.use("/api/auth",authRoute)
 app.use("/api/test", testRoute);
 app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+
 
 
 
